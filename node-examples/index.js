cdd14e19ed3da3df26caae2dc91b474a4ac50c16
luisresende13 <luisresende13@gmail.com>
2021-12-13 03:35:02 -0300

    Initial Commit (Projects Added)

---
+++ node-examples/index.js
@@ -0,0 +1,35 @@
+/* -------------------------
+    var rect = {
+	perimeter: (x, y) => (2*(x+y)),
+	area: (x, y) => (x*y)
+};
+---------------------------  */
+var rect = require('./rectangle');
+
+
+function solveRect(l,b) {
+    
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    
+    const callback = (err,rectangle) => {
+        if (err) {
+            console.log("ERROR: ", err.message);
+            console.log(err)
+        }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    }
+
+    rect(l,b, callback);
+    console.log("This statement after the call to rect()");
+};
+
+solveRect(2,4);
+//solveRect(3,5);
+//solveRect(0,5);
+solveRect(-3,5);    
---
+++ node-http/index.js
@@ -0,0 +1,70 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = 'localhost';
+const port = 3000;
+
+
+const req_res_function = (req, res) => {
+  console.log('Request for ' + req.url + ' by method ' + req.method);
+
+  if (req.method == 'GET') {
+    
+    var fileUrl;
+    if (req.url == '/') fileUrl = '/index.html';
+    else fileUrl = req.url;
+
+    var filePath = path.resolve('./public'+fileUrl);
+    const fileExt = path.extname(filePath);
+    
+    if (fileExt == '.html') {
+      fs.exists(filePath, (exists) => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                      ' not found</h1></body></html>');
+          return;
+        }
+        console.log(req.url)
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res);
+      });
+    }
+    else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body><h1>Error 404: ' + fileUrl + 
+              ' not a HTML file</h1></body></html>');
+    }
+  }
+  else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body><h1>Error 404: ' + req.method + 
+              ' not supported</h1></body></html>');
+  }
+}
+
+
+const server = http.createServer(req_res_function)
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+
+});
+
+/* ------------- SIMPLE SERVER ------------------------------------
+
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+    console.log();
+})
+
+---------------------------  */
